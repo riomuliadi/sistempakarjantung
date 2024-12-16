e7e3dfc014d4a70751e0dd2e70c1f91d10d6d5c3
riomuliadi <rio.muliadi1@gmail.com>
2024-12-16 09:46:50 +0800

    test

--- src/components/HomeTitle.jsx
+++ src/components/HomeTitle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const HomeTitle = () => {
   return (
    <Typography sx={{ marginBottom: 2,  fontSize: 24, fontWeight: 'bold', textAlign: 'center' }}>
-          Selamat Datang Di Sistem Pakar Diagnosa Penyakit Jantung
+          
         </Typography> )
 }
 
