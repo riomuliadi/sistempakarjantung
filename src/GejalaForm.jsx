fa85e0daa0d572ec9d243946b33f90c8b5ada73a
riomuliadi <rio.muliadi1@gmail.com>
2024-12-22 08:54:34 +0700

    test

--- src/GejalaForm.jsx
+++ src/GejalaForm.jsx
@@ -1,158 +1,38 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
 
 import FormDiagnosa from './components/FormDiagnosa';
+import PersistentDrawerLeft from './PersistentDrawer';
 
 const drawerWidth = 240;
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+
 
 export default function GejalaForm() {
+  
+
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerToggle}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Gejala Form
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <Typography
-            sx={{
-              cursor: 'pointer',
-              fontWeight: 'bold',
-              fontSize: 16,
-              padding: 2,
-            }}
-            onClick={handleDrawerToggle}
-          >
-            Toggle Gejala Form
-          </Typography>
-        </Box>
-      </Drawer>
-
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, transition: theme.transitions.create('margin', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: open ? `${drawerWidth}px` : 0 }}
-      >
-        <Toolbar />
-        {open && (
-          <>
-            <Typography
-              sx={{
-                marginBottom: 2,
-                fontSize: 24,
-                fontWeight: 'bold',
-                textAlign: 'center',
-                color: 'black',
-              }}
-            >
-              Silahkan Pilih Gejala Yang Dialami
-            </Typography>
-            <FormDiagnosa />
-          </>
-        )}
-      </Box>
-    </Box>
+    
+    <div className="bg-gray-300 flex-auto">
+    <PersistentDrawerLeft></PersistentDrawerLeft>
+    <Typography color='white' sx={{width: drawerWidth, display: 'flex', marginBottom: 2,  fontSize: 24, fontWeight: 'bold', textAlign: 'center', color:'black'}}>
+    Silahkan Pilih Gejala Yang Dialami
+  </Typography>
+  <FormDiagnosa></FormDiagnosa>
+    </div>
   );
 }
-
-// 
-// import * as React from 'react';
-// import { styled, useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Drawer from '@mui/material/Drawer';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import MuiAppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
-// import List from '@mui/material/List';
-// import Typography from '@mui/material/Typography';
-
-// import FormDiagnosa from './components/FormDiagnosa';
-// import PersistentDrawerLeft from './PersistentDrawer';
-
-// const drawerWidth = 240;
-
-
-
-// export default function GejalaForm() {
-  
-
-//   const theme = useTheme();
-//   const [open, setOpen] = React.useState(false);
-
-
-//   return (
-    
-//     <div className="bg-gray-300 flex-auto">
-//     <PersistentDrawerLeft></PersistentDrawerLeft>
-//     <Typography color='white' sx={{display: 'flex', marginBottom: 2,  fontSize: 24, fontWeight: 'bold', textAlign: 'center', color:'black'}}>
-//     Silahkan Pilih Gejala Yang Dialami
-//   </Typography>
-//   <FormDiagnosa></FormDiagnosa>
-//     </div>
-//   );
-// }
 
 // import React, { useState } from 'react';
 
