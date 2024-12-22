0e9db72ee83efa7d71574a35f79b1592bcab09f7
riomuliadi <rio.muliadi1@gmail.com>
2024-12-22 07:51:46 +0700

    run

--- src/components/HomeTitle.jsx
+++ src/components/HomeTitle.jsx
@@ -1,39 +1,31 @@
 import { Typography, Box } from '@mui/material';
 import React from 'react';
+import PersistentDrawerLeft from '../PersistentDrawer';
 
 const HomeTitle = () => {
   return (
-
     <>
-    <PersistentDrawerLeft></PersistentDrawerLeft>
-    <Typography color='white' sx={{ marginBottom: 2,  fontSize: 24, fontWeight: 'bold', textAlign: 'center', color:'black'}}>
-    Selamat Datang di Sistem Pakar Diagnosa Jantung
-  </Typography>
-  <FormDiagnosa></FormDiagnosa>
+   <PersistentDrawerLeft></PersistentDrawerLeft>
+    <Box 
+      sx={{
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh' // Menggunakan tinggi penuh layar
+      }}
+    >
+      <Typography 
+        color="black" 
+        sx={{ 
+          fontSize: 24, 
+          fontWeight: 'bold', 
+          textAlign: 'center' 
+        }}
+      >
+        Selamat Datang Di Sistem Pakar Diagnosa Penyakit Jantung
+      </Typography>
+    </Box>
     </>
-
-    // <>
-    // <PersistentDrawerLeft></PersistentDrawerLeft>
-    // <Box 
-    //   sx={{
-    //     display: 'flex', 
-    //     justifyContent: 'center', 
-    //     alignItems: 'center', 
-    //     height: '100vh' // Menggunakan tinggi penuh layar
-    //   }}
-    // >
-    //   <Typography 
-    //     color="black" 
-    //     sx={{ 
-    //       fontSize: 24, 
-    //       fontWeight: 'bold', 
-    //       textAlign: 'center' 
-    //     }}
-    //   >
-    //     Selamat Datang Di Sistem Pakar Diagnosa Penyakit Jantung
-    //   </Typography>
-    // </Box>
-    // </>
     
   );
 }
