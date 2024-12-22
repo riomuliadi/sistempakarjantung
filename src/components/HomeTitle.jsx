edd37c4fbe1070636e843ce499ce944e5375ac55
riomuliadi <rio.muliadi1@gmail.com>
2024-12-22 07:46:55 +0700

    run

--- src/components/HomeTitle.jsx
+++ src/components/HomeTitle.jsx
@@ -3,28 +3,37 @@ import React from 'react';
 
 const HomeTitle = () => {
   return (
+
     <>
     <PersistentDrawerLeft></PersistentDrawerLeft>
-    <Box 
-      sx={{
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        height: '100vh' // Menggunakan tinggi penuh layar
-      }}
-    >
-      <Typography 
-        color="black" 
-        sx={{ 
-          fontSize: 24, 
-          fontWeight: 'bold', 
-          textAlign: 'center' 
-        }}
-      >
-        Selamat Datang Di Sistem Pakar Diagnosa Penyakit Jantung
-      </Typography>
-    </Box>
+    <Typography color='white' sx={{ marginBottom: 2,  fontSize: 24, fontWeight: 'bold', textAlign: 'center', color:'black'}}>
+    Selamat Datang di Sistem Pakar Diagnosa Jantung
+  </Typography>
+  <FormDiagnosa></FormDiagnosa>
     </>
+
+    // <>
+    // <PersistentDrawerLeft></PersistentDrawerLeft>
+    // <Box 
+    //   sx={{
+    //     display: 'flex', 
+    //     justifyContent: 'center', 
+    //     alignItems: 'center', 
+    //     height: '100vh' // Menggunakan tinggi penuh layar
+    //   }}
+    // >
+    //   <Typography 
+    //     color="black" 
+    //     sx={{ 
+    //       fontSize: 24, 
+    //       fontWeight: 'bold', 
+    //       textAlign: 'center' 
+    //     }}
+    //   >
+    //     Selamat Datang Di Sistem Pakar Diagnosa Penyakit Jantung
+    //   </Typography>
+    // </Box>
+    // </>
     
   );
 }
