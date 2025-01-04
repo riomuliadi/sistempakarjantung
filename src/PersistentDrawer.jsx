2dafe98e1a5d2ddd8956942c511dd3bea1bfac7c
riomuliadi <rio.muliadi1@gmail.com>
2025-01-04 10:31:15 +0800

    test

--- src/PersistentDrawer.jsx
+++ src/PersistentDrawer.jsx
@@ -1,27 +1,27 @@
-// import * as React from 'react';
-// import { styled, useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Drawer from '@mui/material/Drawer';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import MuiAppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
-// import List from '@mui/material/List';
-// import Typography from '@mui/material/Typography';
-// import Divider from '@mui/material/Divider';
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
-// import { Dashboard, Emergency, ExitToApp, Fullscreen, MedicalInformation } from '@mui/icons-material';
-// import { NavLink, Outlet } from 'react-router-dom';
-// import { blue, grey } from '@mui/material/colors';
-// import { colors } from '@mui/material';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Dashboard, Emergency, ExitToApp, Fullscreen, MedicalInformation } from '@mui/icons-material';
+import { NavLink, Outlet } from 'react-router-dom';
+import { blue, grey } from '@mui/material/colors';
+import { colors } from '@mui/material';
 
 // const drawerWidth = 240;
 
@@ -225,8 +225,8 @@
 //   );
 // }
 
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
+// import Swal from 'sweetalert2';
+// import { useNavigate } from 'react-router-dom';
 
 export default function PersistentDrawerLeft() {
   const theme = useTheme();
